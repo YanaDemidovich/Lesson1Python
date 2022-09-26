cac143892762d1bb0babbe5fd36fc59ea75973d1
Yana Demidovich <demyanvo1990@gmail.com>
2022-09-26 22:28:34 +0300

    Сохранила задачу 3

--- Seminar2/HomeworkTask3.py
+++ Seminar2/HomeworkTask3.py
@@ -1,9 +1,23 @@
 # Задать список из n чисел последовательности (1 + 1/n)^n 
 # и вывести на экран их сумму
 
+# n = int(input('Введите число: '))
+# list = []
+# for i in range(1, n+1):
+#     list.append(round((1 + 1 / i)**i, 2))
+# print(list)
+# print(sum(list))
+
+# или
+
 n = int(input('Введите число: '))
-list = []
+s = 0
+d = {}
+summ = 0
 for i in range(1, n+1):
-    list.append(round((1 + 1 / i)**i, 2))
-print(list)
-print(sum(list))
+    s = (1+1/i)**i
+    d[i] = s
+    summ +=s
+    print(s)
+print(d)
+print(summ)
